5475687137a46f452e084bb882974f3d35ae3fcc
Jhojan <jhojanrock98@gmail.com>
2023-11-28 19:07:06 -0500

    Creación de pantallas y vistas de los elemtos

---
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Authlayout extends StatelessWidget {
+  const Authlayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Authlayout'),
+     )
+    );
+  }
+}
